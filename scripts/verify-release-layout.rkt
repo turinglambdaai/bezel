c3e636c1551e00997d428e2bd824c8a58f12d9c0
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-21 18:47:43 +0800

    test: verify desktop and offscreen Qt platform plugins

--- scripts/verify-release-layout.rkt
+++ scripts/verify-release-layout.rkt
@@ -23,13 +23,16 @@
            "missing ~a under ~a; expected one of ~a"
            label root names)))
 
+(define (require-file label path)
+  (unless (file-exists? path)
+    (error 'verify-release-layout "missing ~a: ~a" label path)))
+
 (case (system-type)
   [(windows)
    (for ([name (in-list '("bezel.dll" "Qt6Core.dll" "Qt6Gui.dll" "Qt6Widgets.dll"))])
-     (unless (file-exists? (build-path root name))
-       (error 'verify-release-layout "missing required Windows runtime file: ~a" name)))
-   (unless (file-exists? (build-path root "platforms" "qwindows.dll"))
-     (error 'verify-release-layout "missing Windows QPA plugin platforms/qwindows.dll"))]
+     (require-file name (build-path root name)))
+   (require-file 'qwindows (build-path root "platforms" "qwindows.dll"))
+   (require-file 'qoffscreen (build-path root "platforms" "qoffscreen.dll"))]
   [(macosx)
    (define frameworks (build-path root "BezelRuntime.app" "Contents" "Frameworks"))
    (define plugins (build-path root "BezelRuntime.app" "Contents" "PlugIns" "platforms"))
@@ -39,13 +42,15 @@
      (error 'verify-release-layout "missing macOS platform plugin directory"))
    (unless (for/or ([name (in-list '("libbezel.0.dylib" "libbezel.dylib"))])
              (file-exists? (build-path frameworks name)))
-     (error 'verify-release-layout "missing libbezel dylib in macOS app bundle"))]
+     (error 'verify-release-layout "missing libbezel dylib in macOS app bundle"))
+   (require-file 'qcocoa (build-path plugins "libqcocoa.dylib"))
+   (require-file 'qoffscreen (build-path plugins "libqoffscreen.dylib"))]
   [else
    (require-any 'libbezel '("libbezel.so.0" "libbezel.so"))
    (require-any 'Qt6Core '("libQt6Core.so.6" "libQt6Core.so"))
    (require-any 'Qt6Gui '("libQt6Gui.so.6" "libQt6Gui.so"))
    (require-any 'Qt6Widgets '("libQt6Widgets.so.6" "libQt6Widgets.so"))
-   (unless (directory-exists? (build-path root "platforms"))
-     (error 'verify-release-layout "missing Linux platform plugin directory"))])
+   (require-file 'qxcb (build-path root "platforms" "libqxcb.so"))
+   (require-file 'qoffscreen (build-path root "platforms" "libqoffscreen.so"))])
 
 (displayln "release runtime layout: OK")
