4622d3730052d7b391293ffed6749495deca7707
TuringLambdaAI <jirentianxiang1024@gmail.com>
2026-09-21 17:38:45 +0800

    Make deployment probe a real Qt executable

--- bezel-shim/tools/deploy_probe.cpp
+++ bezel-shim/tools/deploy_probe.cpp
@@ -1,5 +1,8 @@
+#include <QApplication>
+
 #include <bezel/bezel.h>
 
-int main() {
+int main(int argc, char** argv) {
+    QApplication app(argc, argv);
     return bezel_version() == 1 ? 0 : 1;
 }
